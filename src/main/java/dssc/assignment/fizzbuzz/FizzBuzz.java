e366668cd42a3bdb2584d5616756e5d5f605b0fe
Federico <polli.f@outlook.it>
2020-11-13 17:43:03 +0100

    Added code for FizzBuzz assignment, modified Gradle build in order to run from command line

--- src/main/java/dssc/assignment/fizzbuzz/FizzBuzz.java
+++ src/main/java/dssc/assignment/fizzbuzz/FizzBuzz.java
@@ -3,7 +3,18 @@ package dssc.assignment.fizzbuzz;
 public class FizzBuzz {
 
     public static void main(String... args) {
-        
+        String str;
+        for (int i=1; i<=105; i++) {
+            if (i%3 != 0 && i%5 != 0 && i%7 != 0) {
+                System.out.println(i);
+                continue;
+            }
+            str="";
+            if (i%3 == 0) str+="Fizz";
+            if (i%5 == 0) str+="Buzz";
+            if (i%7 == 0) str+="Bang";
+            System.out.println(str);
+        }
     }
 
 }
